6ef36cd543712834721ce12addb5e999fa8b8dfe
Anwar_Hossain <ranakrphone@gmail.com>
2026-07-09 17:24:20 +0600

    task service updated

--- server/src/services/task.service.ts
+++ server/src/services/task.service.ts
@@ -3,21 +3,33 @@ import { AppError } from "../utils/app-error.js";
 import { Task } from "../generated/client/client.js";
 
 export class TaskService {
-  // ১. নতুন টাস্ক তৈরি করার বিজনেস লজিক
   static async createNewTask(title: string, userId: string): Promise<Task> {
-    // বিজনেস ভ্যালিডেশন
     if (!title.trim()) {
       throw new AppError("Task title cannot be empty", 400);
     }
     return TaskRepository.create(title, userId);
   }
 
-  // ২. আইডি দিয়ে টাস্ক গেট করার লজিক (না পেলে এরর)
   static async getTaskById(id: string): Promise<Task> {
     const task = await TaskRepository.findById(id);
     if (!task) {
       throw new AppError("Requested task was not found", 404);
     }
     return task;
+  }
+
+  // ৩. ইউজারের আইডি অনুযায়ী সব টাস্ক রিটার্ন করার লজিক
+  static async getUserTasks(userId: string): Promise<Task[]> {
+    return TaskRepository.findByUserId(userId);
+  }
+
+  // ৪. টাস্ক স্ট্যাটাস চেক করে ডাটাবেজে পাঠানোর বিজনেস লজিক
+  static async updateTaskStatus(
+    id: string,
+    status: "TODO" | "IN_PROGRESS" | "DONE",
+  ): Promise<Task> {
+    // প্রথমে চেক করে নিব টাস্কটি আদেও এক্সিস্ট করে কি না
+    await this.getTaskById(id);
+    return TaskRepository.updateStatus(id, status);
   }
 }
